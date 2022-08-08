0eb29b5b62a89abdf62cf8b2fd3b54628e7a91f4
TavaresLT <lucastvspvh90@gmail.com>
2022-08-08 08:31:18 -0400

    Seeding Service

---
+++ SalesWebMvc/Migrations/20220805161058_Arrumao.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class Arrumao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "SalesRecords");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "SalesRecords",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
