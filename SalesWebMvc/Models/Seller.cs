a107f2e7bd1131c632ec491d34424d81cb06d80e
TavaresLT <lucastvspvh90@gmail.com>
2022-08-09 09:13:33 -0400

    SellerService and basic FindAll

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,7 +9,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord> ();
@@ -24,7 +24,7 @@ namespace SalesWebMvc.Models
             Id = id;
             Name = name;
             Email = email;
-            Date = date;
+            BirthDate = date;
             BaseSalary = baseSalary;
             Department = department;
         }
