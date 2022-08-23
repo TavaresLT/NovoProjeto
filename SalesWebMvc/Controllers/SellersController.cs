7cddeabb7019ed039af1fbb84517742272b8dcc7
TavaresLT <lucastvspvh90@gmail.com>
2022-08-23 10:05:50 -0400

    Single Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using SalesWebMvc.Services;
-
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -23,6 +19,19 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) 
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction("Index");
         }
     }
 }
